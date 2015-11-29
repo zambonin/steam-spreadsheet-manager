0213885672e4041b18d6c6cc3d93072601f65694
Gustavo Zambonin <gzmbnn@gmail.com>
2015-11-29 03:30:13 -0200

    doesn't write anything to disk now * better use of string formatting

--- manager.py
+++ manager.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import csv
-import datetime
 import gspread
 import json
 import locale
@@ -9,6 +8,7 @@ import re
 import requests
 import subprocess
 
+from datetime import datetime
 from oauth2client.service_account import ServiceAccountCredentials
 
 
@@ -24,10 +24,10 @@ def merge_dict_lists(list1, list2, key):
 
 def show_icon(game):
     if "img_icon_url" in game.keys():
-        return "=IMAGE(\"http://media.steampowered.com/steamcommunity/" + \
-            "public/images/apps/%d/%s.jpg\"; 1)" % (game['appid'],
-                                                    game['img_icon_url'])
-
+        return ("=IMAGE(\"http://media.steampowered.com/"
+                "steamcommunity/public/images/"
+                "apps/{}/{}.jpg\"; 1)").format(game['appid'],
+                                               game['img_icon_url'])
     return ""
 
 
@@ -59,89 +59,71 @@ def discount_info(game):
 
 
 def achiev_info(game):
-    url = "http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/"
+    url = ("http://api.steampowered.com/ISteamUserStats/"
+           "GetPlayerAchievements/v0001/")
     data = requests.get(url, params={
-                                "appid": str(game['appid']),
-                                "key": api_key,
-                                "steamid": steamid,
-                            }).json()['playerstats']
+                        "appid": str(game['appid']),
+                        "key": api_key,
+                        "steamid": steamid,
+                        }).json()['playerstats']
 
-    game['achiev'] = ""
     if data['success'] and 'achievements' in data.keys():
         cont = len([i for i in data['achievements'] if i['achieved']])
         game['achiev'] = cont / len(data['achievements'])
-
-    if type(game['achiev']) == str:
-        return ""
-    return locale.str(game['achiev'])
+        return locale.str(game['achiev'])
+    return ""
 
 
-def license_info(game):
-    for each in license_data:
-        if str(game['appid']) in each['apps']:
-            game['package'] = each['package']
-            game['date'] = each['date'].strftime('%d/%m/%Y %H:%M:%S')
-            game['location'] = each['location']
-            game['license'] = each['license']
-    return game
-
-
-def read_steam_data():
-    url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v1/"
-    return requests.get(url, params={
-                                "key": api_key,
-                                "steamid": steamid,
-                                "include_played_free_games": 1,
-                                "include_appinfo": 1,
+def prep_game_list():
+    def read_steam_data():
+        url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v1/"
+        return requests.get(url, params={
+                            "key": api_key,
+                            "steamid": steamid,
+                            "include_played_free_games": 1,
+                            "include_appinfo": 1,
                             }).json()['response']['games']
 
+    def read_price_data(path):
+        with open(path) as f:
+            content = csv.reader(f)
+            return [{'appid': int(line[0]), 'price_paid': float(line[1])}
+                    for line in content]
 
-def read_price_data(path):
-    prices = []
-    with open(path) as f:
-        content = csv.reader(f)
-        for line in content:
-            prices.append({
-                'appid': int(line[0]),
-                'price_paid': float(line[1]),
-            })
+    def read_license_data():
+        cmd = "steamcmd +login {} +licenses_print +quit".format(
+            steam_login).split()
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        content = [i.decode() for i in proc.stdout]
+        index = [i for i, line in enumerate(content) if "License" in line][1]
 
-    return prices
+        return [{'package': int(re.findall(r'(\d+)', content[i])[0]),
+                 'date': datetime.strptime(re.findall(
+                     r'.* : (.+?) in .*', content[i+1])[0],
+                     '%a %b %d %H:%M:%S %Y'),
+                 'location': re.findall(r'"(.*?)"', content[i+1])[0],
+                 'license': re.findall(r'.*\, (.*)', content[i+1])[0],
+                 'apps': re.findall(r'(\d+)', content[i+2])[:-1]}
+                for i in range(index, len(content), 4)]
 
+    steam_data = read_steam_data()
+    price_data = read_price_data('prices.csv')
+    license_data = read_license_data()
 
-def read_license_data():
-    bashCommand = "steamcmd +login " + steam_login + \
-              " +licenses_print +quit > licenses.txt"
-    subprocess.Popen(bashCommand, shell=True, stdout=subprocess.PIPE)
-    license_info = []
-    with open('licenses.txt') as f:
-        data = f.readlines()
-        index = 0
-        for i in range(len(data)):
-            if "License" in data[i]:
-                index = i+4
+    priced_games = merge_dict_lists(steam_data, price_data, 'appid')
+    freeless = [i for i in priced_games if 'price_paid' in i.keys()]
+
+    for g in freeless:
+        for l in license_data:
+            if str(g['appid']) in l['apps']:
+                g['package'] = l['package']
+                g['date'] = l['date'].strftime('%d/%m/%Y %H:%M:%S')
+                g['location'] = l['location']
+                g['license'] = l['license']
                 break
 
-        for i in range(index, len(data), 4):
-            if "License" in data[i]:
-                license_info.append({
-                    'package': int(re.findall(r'(\d+)', data[i])[0]),
-                    'date': datetime.datetime.strptime(re.findall(
-                                r'.* : (.+?) in .*', data[i+1])[0],
-                                '%a %b %d %H:%M:%S %Y'),
-                    'location': re.findall(r'"(.*?)"', data[i+1])[0],
-                    'license': re.findall(r'.*\, (.*)', data[i+1])[0],
-                    'apps': re.findall(r'(\d+)', data[i+2])[:-1],
-                })
+    return sorted(freeless, key=lambda k: k['appid'])
 
-    return license_info
-
-
-def exclude_free_games(game_list):
-    for game in game_list:
-        if 'price_paid' not in game.keys():
-            game_list.remove(game)
-    return game_list
 
 private_data = json.load(open('config.json'))
 api_key = private_data['api_key']
@@ -154,21 +136,15 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name(
 
 gc = gspread.authorize(credentials)
 
-prices = read_price_data('prices.csv')
-game_data = read_steam_data()
-game_list = exclude_free_games(sorted(merge_dict_lists(
-    game_data, prices, 'appid'), key=lambda k: k['appid']))
-license_data = read_license_data()
+game_list = prep_game_list()
 
 locale.setlocale(locale.LC_ALL, '')
 worksheet = gc.open_by_key(private_data['spreadsheet_key']).sheet1
-index = 2
-worksheet.resize(len(game_list) + (index - 1))
+index, length = 2, len(game_list)
+worksheet.resize(length + (index - 1))
 values_list = []
 
-for game, i in zip(game_list, range(index, len(game_list) + index)):
-    game = license_info(game)
-
+for game, i in zip(game_list, range(index, length + index)):
     url = "http://store.steampowered.com/api/appdetails/"
     raw = requests.get(url, params={"appids": str(game['appid'])}).json()
     data = raw[list(raw.keys())[0]]
@@ -185,12 +161,11 @@ for game, i in zip(game_list, range(index, len(game_list) + index)):
                     achiev_info(game), discount_info(game), game['package'],
                     game['date'], game['location'], game['license']]
 
-    percentage = (i - index + 1) / len(game_list)
-    print("[{:2.1%}] ".format((i - index + 1) / len(game_list)) +
-          "Row #%d" % i, end='\r')
+    percentage = ((i - index + 1) / length)
+    print("[{:2.1%}] Row #{}".format(percentage, i), end='\r')
 
 print("Uploading to Google Drive...")
-cell_list = worksheet.range('A%d:L%d' % (index, len(game_list) + (index - 1)))
+cell_list = worksheet.range('A{}:L{}'.format(index, length + index - 1))
 
 for cell, value in zip(cell_list, values_list):
     cell.value = value
